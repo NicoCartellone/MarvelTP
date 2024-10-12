1bf12262b2180f6673b3b2cdf8bd8443b20017eb
Nicolás Cartellone <ncartellone@alumno.unlam.edu.ar>
2024-10-12 20:35:28 -0300

    add: navigation component

--- composeApp/src/commonMain/kotlin/com/necs/marveltp/ui/navigation/Navigation.kt
+++ composeApp/src/commonMain/kotlin/com/necs/marveltp/ui/navigation/Navigation.kt
@@ -1,9 +1,12 @@
 package com.necs.marveltp.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.necs.marveltp.data.models.characters
 import com.necs.marveltp.ui.screens.characterDetail.CharacterDetailScreen
 import com.necs.marveltp.ui.screens.home.HomeScreen
 
@@ -12,10 +15,21 @@ fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "HomeScreen") {
         composable("HomeScreen") {
-            HomeScreen(onClick = { navController.navigate("CharacterDetail") })
+            HomeScreen(
+                onCharacterClick = { character ->
+                    navController.navigate("CharacterDetail/${character.id}")
+                }
+            )
         }
-        composable("CharacterDetail") {
-            CharacterDetailScreen(onClick = { navController.navigate("HomeScreen") })
+        composable(
+            route = "CharacterDetail/{characterId}",
+            arguments = listOf(navArgument("characterId") { type = NavType.IntType })
+        ) { backstackEntry ->
+            val characterId = backstackEntry.arguments?.getInt("characterId")
+            CharacterDetailScreen(
+                character = characters.first { it.id.toInt() == characterId },
+                onBack = { navController.popBackStack() }
+            )
         }
     }
 }
