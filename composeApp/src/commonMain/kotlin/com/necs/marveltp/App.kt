e0c13fba94d850474171b5e37e02f7c48a57856d
Nicolás Cartellone <ncartellone@alumno.unlam.edu.ar>
2024-10-12 20:31:36 -0300

    add: coil image load builder

--- composeApp/src/commonMain/kotlin/com/necs/marveltp/App.kt
+++ composeApp/src/commonMain/kotlin/com/necs/marveltp/App.kt
@@ -1,14 +1,23 @@
 package com.necs.marveltp
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
 import com.necs.marveltp.ui.navigation.Navigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Navigation()
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .build()
     }
+    Navigation()
 }
