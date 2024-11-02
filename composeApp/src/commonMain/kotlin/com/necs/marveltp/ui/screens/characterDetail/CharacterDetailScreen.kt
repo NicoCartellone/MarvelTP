3b9e597721a6035f8bad4eac714be9083da3f0a4
Nicolás Cartellone <ncartellone@alumno.unlam.edu.ar>
2024-11-02 01:48:17 -0300

    refactor: Clean CharacterDetailScreen

--- composeApp/src/commonMain/kotlin/com/necs/marveltp/ui/screens/characterDetail/CharacterDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/necs/marveltp/ui/screens/characterDetail/CharacterDetailScreen.kt
@@ -2,6 +2,7 @@ package com.necs.marveltp.ui.screens.characterDetail
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,12 +17,15 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import com.necs.marveltp.data.models.Character
 import com.necs.marveltp.ui.screens.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,40 +35,67 @@ fun CharacterDetailScreen(
     onBack: () -> Unit,
 ) {
     val state = vm.state
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Screen {
         Scaffold(topBar = {
-            TopAppBar(title = { Text(state.character?.name ?: "") }, navigationIcon = {
-                IconButton(onClick = { onBack() }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                        contentDescription = "Back"
-                    )
-                }
-            })
+            CharacterDetailTopBar(
+                title = state.character?.name ?: "",
+                onBack = onBack,
+                scrollBehavior = scrollBehavior
+            )
         }) { padding ->
-            if(state.loading){
+            if (state.loading) {
                 Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                    modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                 ) {
                     CircularProgressIndicator()
                 }
             }
             state.character?.let { character ->
-                Column(
-                    modifier = Modifier.fillMaxSize().padding(padding)
-                        .verticalScroll(rememberScrollState()),
-                ) {
-                    AsyncImage(
-                        model = character.thumbnailUrl,
-                        contentDescription = "Character image",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize().aspectRatio(16 / 9f)
-                    )
-                    Text(text = character.description, modifier = Modifier.padding(8.dp))
-                }
+                CharacterDetail(
+                    modifier = Modifier.padding(padding),
+                    character = character
+                )
             }
         }
     }
+}
+
+@Composable
+private fun CharacterDetail(
+    modifier: Modifier = Modifier,
+    character: Character,
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+    ) {
+        AsyncImage(
+            model = character.thumbnailUrl,
+            contentDescription = "Character image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize().aspectRatio(16 / 9f)
+        )
+        Text(
+            text = character.description,
+            modifier = Modifier.padding(16.dp))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun CharacterDetailTopBar(
+    title: String,
+    onBack: () -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior,
+) {
+    TopAppBar(title = { Text(title) }, navigationIcon = {
+        IconButton(onClick = { onBack() }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Back"
+            )
+        }
+    }, scrollBehavior = scrollBehavior
+    )
 }
